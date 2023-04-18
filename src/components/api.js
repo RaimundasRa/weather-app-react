6f9f00ad1cde14d43236909429148c0102758450
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-04-18 08:04:11 +0100

    search component

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '61ed8c2129msh49d103fdd650eb3p136a0ajsn11411fd128f9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
