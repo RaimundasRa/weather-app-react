cefd40b02d07c19d11553baf3be269041707a076
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-04-19 15:10:37 +0100

    basic styling for top part of the component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -2,12 +2,14 @@ import './current-weather.css'
 
 const CurrentWeather = () => {
   return (
-    <div className='wather'>
+    <div className='weather'>
       <div className='top'>
-        <p className='city'>London</p>
-        <p className='weather-description'>Sunny</p>
+        <div className=''>
+          <p className='city'>London</p>
+          <p className='weather-description'>Sunny</p>
+        </div>
+        <img alt='weather' className='weather-icon' src='icons/01d.png' />
       </div>
-      <img alt='weather' className='weather-icon' src='icons/01d.png' />
     </div>
   );
 }
