4d2368f83d4676fef6e2b79af487195155de1485
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-04-19 14:44:10 +0100

    added icons & basic structure to weather component

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,7 +1,15 @@
 import './current-weather.css'
 
 const CurrentWeather = () => {
-  return "hello"
+  return (
+    <div className='wather'>
+      <div className='top'>
+        <p className='city'>London</p>
+        <p className='weather-description'>Sunny</p>
+      </div>
+      <img alt='weather' className='weather-icon' src='icons/01d.png' />
+    </div>
+  );
 }
 
 export default CurrentWeather;
